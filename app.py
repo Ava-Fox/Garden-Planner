9144d33369c07b31b81cebb5d2aa67097cac4f17
avafox <avafoxemail@gmail.com>
2023-12-08 15:19:24 -0700

    Initialize garden app

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+from cs50 import SQL
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
