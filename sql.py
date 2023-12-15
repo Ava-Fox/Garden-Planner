9feb71c2f519e6738b8299dd1444110020a7486d
avafox <avafoxemail@gmail.com>
2023-12-15 15:43:21 -0700

    Fix sql.py (Wasn't adding up to correct plots; it is now)

--- sql.py
+++ sql.py
@@ -10,15 +10,15 @@ from cs50 import SQL
 db = SQL("sqlite:///garden.db")
 
 # Start w/ the 4X4
-#for y in range(1,5):
-#    for x in range(1, 5):
-#        # add plot w/ local_x as x and local_y as y and bed = 3
-#        db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, 3)
+for y in range(1,5):
+    for x in range(1, 5):
+        # add plot w/ local_x as x and local_y as y and bed = 3
+        db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, 3)
 
 # Now the 4x8
-#for y in range(1, 5):
-#    for x in range(1, 9):
-#       db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, 6)
+for y in range(1, 5):
+    for x in range(1, 9):
+       db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, 6)
 
 # Now the L's
 for b in range(1, 5):
@@ -32,12 +32,32 @@ for b in range(1, 5):
             else:
                  # create 4X8
                 for x in range(1, 9):
-                    db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?)", x, y, b)
+                    db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, b)
               
     elif b == 2:
-        
-        continue
+        for y in range(1, 9):
+            if y > 4:
+                # Create 4x4 on right side
+                for x in range(5, 9):
+                    db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, b)
+            else:
+                for x in range(1, 9):
+                    db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, b)
+
     elif b == 3:
-        continue
+        for y in range(1, 9):
+            if y <= 4:
+                for x in range(1, 5):
+                    db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, 4)
+            else:
+                for x in range(1, 9):
+                    db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, 4)
+
     elif b == 4:
-        continue
+        for y in range(1, 9):
+            if y <= 4:
+                for x in range(5, 9):
+                    db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, 5)
+            else:
+                for x in range(1, 9):
+                    db.execute("INSERT INTO plot (local_x, local_y, bed) VALUES (?, ?, ?);", x, y, 5)
